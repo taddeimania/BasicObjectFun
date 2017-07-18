3ff2a6093a6fcd5154535547d3819a8186428a37
taddeimania <jtaddei@gmail.com>
2017-07-18 11:36:41 -0400

    Started library classes

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Car crv = new Car("v8 hemi");
+        crv.setColor("Purple");
+
+        System.out.println(crv.getColor());
     }
 }
